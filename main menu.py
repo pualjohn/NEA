a2a759548c127cc80e25a7e4709bf78840d66c1d
pualjohn <ipayurbillslooll@gmail.com>
2022-03-27 14:42:26 +0100

    shem

---
+++ main menu.py
@@ -0,0 +1,27 @@
+from _typeshed import ReadableBuffer
+import pygame, sys, os
+from pygame.locals import *
+pygame.init()
+#sound effects 
+
+
+#colours
+BLACK='#000000'
+GREY='#555555'
+WHITE='#FFFFFF'
+RED='#FF0000'
+BLUE='#0000FF'
+GREEN='#00FF00'
+YELLOW='#FFFF00'
+PINK='#FF00FF'
+CYAN= '#00FFFF'
+BACKGROUND_COLOUR=GREY
+#VIDEO
+FPS = 30
+window_width,window_height=640,480
+screen=pygame.display.set_mode((window_width,window_height))
+clock= pygame.time.Clock()
+
+#background pic
+Background_pic=
+
